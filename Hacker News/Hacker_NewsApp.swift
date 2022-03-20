bab376f1eac4e694c4f8d01294b088428e47ce75
NataliyaLaz <nataliya.laz@gmail.com>
2022-03-20 10:50:01 +0300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Nataliya Lazouskaya on 20.03.22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
